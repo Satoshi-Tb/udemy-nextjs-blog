79f592b603236ce76efd0fd3035c47a68315df9c
Satoshi T <sulp91_fy@yahoo.co.jp>
2021-10-05 07:55:08 +0900

    ログイン、ログアウト機能実装

--- components/Auth.js
+++ components/Auth.js
@@ -1,4 +1,68 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Cookie from "universal-cookie";
+
+const cookie = new Cookie();
+
 export default function Auth() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const login = async () => {
+    try {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+        {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          } else if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const options = { path: "/" };
+          cookie.set("access_token", data.access, options);
+        });
+      router.push("/main-page");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const authUser = async (e) => {
+    e.preventDefault(); //再読み込みを防ぐため、イベント動作を抑止する
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+
   return (
     <div className="max-w-md w-full space-y-8">
       <div>
@@ -7,9 +71,11 @@ export default function Auth() {
           src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
           alt="Workflow"
         />
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-white">Sign in you account</h2>
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
+          {isLogin ? "Login" : "Sign up"}
+        </h2>
       </div>
-      <form className="mt-8 space-y-6">
+      <form className="mt-8 space-y-6" onSubmit={authUser}>
         <input type="hidden" name="remember" value="true" />
         <div className="rounded-md shadow-sm -space-y-px">
           <div>
@@ -20,6 +86,8 @@ export default function Auth() {
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Username"
+              value={username}
+              onChange={(e) => {setUsername(e.target.value)}}
             />
           </div>
           <div>
@@ -30,13 +98,15 @@ export default function Auth() {
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Password"
+              value={password}
+              onChange={(e) => {setPassword(e.target.value)}}
             />
           </div>
         </div>
 
         <div className="flex items-center justify-center">
           <div className="text-sm">
-            <span className="cursor-pointer font-medium text-white hover:text-indigo-500">
+            <span onClick={() => setIsLogin(!isLogin)} className="cursor-pointer font-medium text-white hover:text-indigo-500">
               change mode ?
             </span>
           </div>
@@ -62,6 +132,7 @@ export default function Auth() {
                 />
               </svg>
             </span>
+            {isLogin ? "Login with JWT" : "Create new user"}
           </button>
         </div>
       </form>
